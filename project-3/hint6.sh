396e73540e90d5b6f6ddca5cd33986069972ecfb
dipoesan <esan.dipo@gmail.com>
2022-01-17 03:49:52 +0100

    added extra steps and rectified issues on step 3

--- project-3/hint6.sh
+++ project-3/hint6.sh
@@ -7,7 +7,7 @@ seconds_sofar=$1
 # echo "Debug Hint Task 2: $seconds_sofar"
 
 # This hint message will appear between 10-20 seconds elapsed after the task began.
-if [[ $seconds_sofar -ge 10 &&  $seconds_sofar -lt 20 ]]; then
+if [[ $seconds_sofar -ge 7 &&  $seconds_sofar -lt 15 ]]; then
   echo "Still working on it? I could help out if needed..."
 fi
 
--- project-3/step3.md
+++ project-3/step3.md
@@ -1,5 +1,4 @@
->>Which of these commands can you use to download files from the web?   <<
-[ ] cat
-[ ] grep
-[ ] pull
-[*] wget
+In your current directory, there is a folder called `darey.io`. Navigate into it and download `http://darey.io/`.
+
+>> What is the name of the downloaded file? <<
+=== index.html
